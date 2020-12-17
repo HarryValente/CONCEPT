cfcaf104bec5738f6e8b4a90b0be9f5294a3aca6
HarryValente <harry-joevalente@live.co.uk>
2020-12-17 23:22:34 +0000

    Fixed nav with a new colour scheme

--- kitchenware.js
+++ kitchenware.js
@@ -1,34 +1,34 @@
 window.addEventListener("scroll", function(){
-    let header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+    let nav = document.querySelector("nav");
+    nav.classList.toggle("sticky", window.scrollY > 0);
 })
 
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    //toggle nav
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-links');
+//     const navLinks = document.querySelectorAll('.nav-links li');
+//     //toggle nav
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
+//     burger.addEventListener('click', () => {
+//         nav.classList.toggle('nav-active');
         
-        //animate links
+//         //animate links
 
-    navLinks.forEach((link, index) => {
-        if(link.style.animation) {
-            link.style.animation = ''
-        }     else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
-        }
-    });
-    //burger animation
-    burger.classList.toggle('toggle');
+//     navLinks.forEach((link, index) => {
+//         if(link.style.animation) {
+//             link.style.animation = ''
+//         }     else {
+//                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+//         }
+//     });
+//     //burger animation
+//     burger.classList.toggle('toggle');
 
-    });
+//     });
     
-}
+// }
     
-navSlide();
+// navSlide();
 
 
 const filterSlide = () => {
