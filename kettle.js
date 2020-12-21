67984e72f5728ac3baf34905dc46eb323223d402
HarryValente <harry-joevalente@live.co.uk>
2020-12-21 23:04:34 +0000

    Layout organised for more content
    Next job is to fill the container with content

--- kettle.js
+++ kettle.js
@@ -2,3 +2,15 @@ window.addEventListener("scroll", function(){
     let nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
 })
+
+const description = document.querySelector('.description')
+const description_list = document.querySelector('.Des-list')
+
+
+description.addEventListener('click', () => {
+    description_list.classList.toggle('active')
+})
+
+// description.addEventListener('click', () => {
+//     description_list.classList.remove('active')
+// })
